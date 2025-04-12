a2564938d77a12fcca9f473e42e02b3a6765bdb8
Santos23231 <159680599+Santos23231@users.noreply.github.com>
2025-04-11 19:16:47 -0600

    COMIIT
    HOLAAAA CAMBIOS

--- index.php
+++ index.php
@@ -34,12 +34,6 @@
     <div class="container-fluid">
     <a class="navbar-brand" href="#">Biblioteca</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-    </ul>
-    </div>
     <button class="btn btn-primary" onclick="window.location.href='ingresar_libro.php';">Ingresar un nuevo libro</button>
     </div>
     </nav>
